06cf9a5b52e3a76776d43dc4439bb974e96e6aca
Walmyr <wlsf82@gmail.com>
2021-06-10 00:39:26 +0200

    Solve lesson 1 exercise 1

--- cypress/integration/browser-testing/sample1.spec.js
+++ cypress/integration/browser-testing/sample1.spec.js
@@ -4,8 +4,8 @@ describe('Browser testing bad practice - Sample 1', () => {
   })
 
   it('directs the user to the login page when clicking the login link', () => {
-    cy.contains('.nav a', 'Login').click()
-
-    cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
+    cy.contains('.nav a', 'Login')
+      .should('have.attr', 'href', '/login')
+      .and('not.have.attr', 'target')
   })
 })
