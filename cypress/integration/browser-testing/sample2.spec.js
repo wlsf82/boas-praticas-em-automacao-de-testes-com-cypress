f6c4912da8f032b277b68d4f371359b1f548caf7
Walmyr <wlsf82@gmail.com>
2021-06-10 00:59:57 +0200

    Solve lesson 1 extra credit 1

--- cypress/integration/browser-testing/sample2.spec.js
+++ cypress/integration/browser-testing/sample2.spec.js
@@ -1,13 +1,19 @@
 describe('Browser testing bad practice', () => {
   beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('getStories')
+
     cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
   })
 
   it('tests a browser feature instead of an app feature', () => {
     cy.get('.table-row a')
       .first()
-      .click()
-
-    // Assert that a new tab was opened in the correct URL
+      .should('have.attr', 'href', 'https://example.com/lc-jg')
+      .and('have.attr', 'target', '_blank')
   })
 })
