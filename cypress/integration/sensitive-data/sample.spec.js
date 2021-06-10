d5f5b0b040816e9232cffdf620d00556076ddc6e
Walmyr <wlsf82@gmail.com>
2021-06-10 03:02:28 +0200

    Solve lesson 7 exercise

--- cypress/integration/sensitive-data/sample.spec.js
+++ cypress/integration/sensitive-data/sample.spec.js
@@ -4,7 +4,7 @@ describe('Sensitive data bad practice', () => {
   })
 
   it('fills the form with sensitive data', () => {
-    cy.get('#email').type('joe@example.com')
-    cy.get('#password').type('s3Cr€7-p@s5w0rd')
+    cy.get('#email').type(Cypress.env('user_email'))
+    cy.get('#password').type(Cypress.env('user_password'), { log: false })
   })
 })
