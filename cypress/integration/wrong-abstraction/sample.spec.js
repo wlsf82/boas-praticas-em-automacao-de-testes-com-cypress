0e1aa192530393ebf98bcf5b9c9aff31dc8ad9df
Walmyr <wlsf82@gmail.com>
2021-06-10 03:48:58 +0200

    Solve lesson 10 extra credit 1

--- cypress/integration/wrong-abstraction/sample.spec.js
+++ cypress/integration/wrong-abstraction/sample.spec.js
@@ -13,26 +13,20 @@ describe('Wrong abstraction bad practice', () => {
     cy.search('cypress')
     cy.wait('@getStories')
 
-    cy.get('.table-row').then(rows => {
-      expect(rows.length).to.be.at.least(1)
-    })
+    cy.get('.table-row').its('length').should('be.at.least', 1)
   })
 
   it('uses custom command for assertion just for the sake of reusability', () => {
     cy.search('selenium')
     cy.wait('@getStories')
 
-    cy.get('.table-row').then(rows => {
-      expect(rows.length).to.be.at.least(1)
-    })
+    cy.get('.table-row').its('length').should('be.at.least', 1)
   })
 
   it('uses custom command for assertion just for the sake of reusability', () => {
     cy.search('playwright')
     cy.wait('@getStories')
 
-    cy.get('.table-row').then(rows => {
-      expect(rows.length).to.be.at.least(1)
-    })
+    cy.get('.table-row').its('length').should('be.at.least', 1)
   })
 })
