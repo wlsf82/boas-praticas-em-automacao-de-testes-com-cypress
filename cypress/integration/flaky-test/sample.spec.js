ca4b70d7b59a72783e6e64556d46d3effd6a54e4
Walmyr <wlsf82@gmail.com>
2021-06-10 02:17:11 +0200

    Solve lesson 3 extra credit 2

--- cypress/integration/flaky-test/sample.spec.js
+++ cypress/integration/flaky-test/sample.spec.js
@@ -7,7 +7,6 @@ describe('Flaky tests bad practice', () => {
     ).as('getStories')
 
     cy.visit('https://wlsf82-hacker-stories.web.app')
-    cy.wait('@getStories')
   })
 
   Cypress._.times(10, () => {
@@ -17,8 +16,6 @@ describe('Flaky tests bad practice', () => {
       Cypress._.times(6, () => {
         cy.search(faker.random.word())
       })
-
-      cy.wait('@getStories')
 
       cy.get('.last-searches button')
         .should('have.length', 5)
