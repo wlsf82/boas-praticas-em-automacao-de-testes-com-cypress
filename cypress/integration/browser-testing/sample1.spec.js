4a0c2c5435268b64b65398bcd384bc3dfc52feb5
Adriano <adrianoaluizi@hotmail.com>
2022-06-29 11:17:16 -0300

    Browser testing good practice

--- cypress/integration/browser-testing/sample1.spec.js
+++ cypress/integration/browser-testing/sample1.spec.js
@@ -4,8 +4,11 @@ describe('Browser testing bad practice - Sample 1', () => {
   })
 
   it('directs the user to the login page when clicking the login link', () => {
-    cy.contains('.nav a', 'Login').click()
+    cy.contains('.nav a', 'Login')
+      .should('have.attr', 'href', '/login')
+      .and('not.have.attr', 'target')
 
-    cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
+    // Má prática  
+    // cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
   })
 })
--- cypress/integration/browser-testing/sample2.spec.js
+++ cypress/integration/browser-testing/sample2.spec.js
@@ -1,12 +1,20 @@
 describe('Browser testing bad practice', () => {
   beforeEach(() => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('getStories')
+
     cy.visit('https://hackernews-seven.vercel.app')
+    cy.wait('@getStories')
   })
 
   it('tests a browser feature instead of an app feature', () => {
     cy.get('.table-row a')
       .first()
-      .click()
+      .should('have.attr', 'href', 'https://example.com/lc-jg')
+      .and('have.attr', 'target', '_blank')
 
     // Assert that a new tab was opened in the correct URL
   })
